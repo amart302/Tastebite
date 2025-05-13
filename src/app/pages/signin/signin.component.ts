f46cfbc36ee1c0ff4dd6f3c6e11610cb11c9cbde
Ahmed <amartazanov892@gmail.com>
2025-05-13 22:59:25 +0300

    commit 1

---
+++ src/app/pages/signin/signin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-signin',
+  imports: [RouterLink],
+  templateUrl: './signin.component.html',
+  styleUrl: './signin.component.scss'
+})
+export class SigninComponent {
+
+}
